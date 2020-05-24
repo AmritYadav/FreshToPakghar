0ae48c69bd2cf67610d972913a17923c968f24b1
AmritYadav <amrity5@gmail.com>
2020-05-24 19:56:10 +0530

    Details Fragment

--- app/src/main/java/com/ftp/ui/grocery/adapters/ProductAdapter.kt
+++ app/src/main/java/com/ftp/ui/grocery/adapters/ProductAdapter.kt
@@ -4,7 +4,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.ftp.ui.grocery.models.Product
 
-class ProductAdapter(private val products: List<Product>) :
+class ProductAdapter(private val products: List<Product>, private val callback: (product: Product) -> Unit) :
     RecyclerView.Adapter<ProductViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder =
@@ -13,7 +13,7 @@ class ProductAdapter(private val products: List<Product>) :
     override fun getItemCount(): Int = products.size
 
     override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
-        holder.bind(products[position])
+        holder.bind(products[position], callback)
     }
 
 }
