5bc03b27520ff1232b7e31d7b5b9f4a8fafe5d39
aniketbhoite <aniketbhoite1203@gmail.com>
2020-05-25 17:19:18 +0530

    User model added and category model annotations

--- app/src/main/java/com/ftp/ui/dashboard/models/Category.kt
+++ app/src/main/java/com/ftp/ui/dashboard/models/Category.kt
@@ -1,5 +1,22 @@
 package com.ftp.ui.dashboard.models
 
 import androidx.annotation.DrawableRes
+import com.google.gson.annotations.SerializedName
 
-data class Category(val id: Int, @DrawableRes val imageUrl: Int, val name: String)
+data class Category(
+    @SerializedName("id")
+    val id: Int,
+
+    @SerializedName("thumb")
+    @DrawableRes
+    val imageUrl: Int,
+
+    @SerializedName("name")
+    val name: String,
+
+    @SerializedName("is_active")
+    val isActive: Int = 1,
+
+    @SerializedName("pref_type")
+    val prefType: Int = 1
+)
